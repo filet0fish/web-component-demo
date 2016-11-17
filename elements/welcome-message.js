d20ea8425d3630bb4adf00ffea901b6d10ed2695
Josh Trout <jtrout@gannett.com>
2016-11-16 21:08:08 -0500

    with shadow dom

--- elements/welcome-message.js
+++ elements/welcome-message.js
@@ -2,10 +2,16 @@ class WelcomeClass extends HTMLElement {
 
     constructor() {
         super();
+
+        let shadowRoot = this.attachShadow({mode: 'open'});
+        shadowRoot.innerHTML = `
+            <style>
+                h1 { color: rebeccapurple; }
+            </style>
+            <h1>Hello, shadow dom!</h1>
+        `;
     }
 
-    connectedCallback() {
-        this.innerHTML = "Hello, Web Components";
-    }
+    connectedCallback() {}
 }
 window.customElements.define('welcome-message', WelcomeClass);
