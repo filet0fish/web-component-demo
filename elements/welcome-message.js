bfd34db7205dba956fec7a81ac792588426370ab
Josh Trout <josh.trout@ironnetcybersecurity.com>
2019-03-31 13:42:12 -0400

    Fix bug, add all lifecycle methods

--- elements/welcome-message.js
+++ elements/welcome-message.js
@@ -2,7 +2,26 @@ class WelcomeClass extends HTMLElement {
 
     constructor() {
         super();
+        // Class is instantiated
+    }
+
+    connectedCallback() {
+        // Element is attached to the DOM
         this.innerHTML = "Hello, Web Components";
     }
+
+    disconnectedCallback() {
+        // Element is removed from the DOM
+    }
+
+    adoptedCallback() {
+        // Element is moved to a new document
+    }
+
+    attributeChangedCallback() {
+        // An attribute change on the element
+    }
+
+    static get observedAttributes() { return ['class', 'message']; }
 }
 window.customElements.define('welcome-message', WelcomeClass);
