d5bb2b9f00552e718411fd292b5d7d4fa6038daf
Josh Trout <josh.trout@ironnetcybersecurity.com>
2019-03-31 13:43:28 -0400

    Put in connectedCallback

--- elements/welcome-message.js
+++ elements/welcome-message.js
@@ -2,7 +2,9 @@ class WelcomeClass extends HTMLElement {
 
     constructor() {
         super();
+    }
 
+    connectedCallback() {
         let templateElement = document.getElementById('message');
         let ourCopy = document.importNode(templateElement.content, true); // True == copy nodes
         this.appendChild(ourCopy);
